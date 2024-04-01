b36dec0b9fbe2cc4e5a5f478260a460e980b87ab
Radek Oleński <radek.olenski@somethingrandom.com>
2024-04-01 22:13:45 +0200

    Add separate react client project

--- StockApp/Program.cs
+++ StockApp/Program.cs
@@ -2,21 +2,14 @@
 // Copyright (c) Radosław Oleński. All rights reserved
 // </copyright>
 
-using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
-using JavaScriptEngineSwitcher.V8;
-using React.AspNet;
-
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
 services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-services.AddReact();
-services.AddJsEngineSwitcher(static options => { options.DefaultEngineName = V8JsEngine.EngineName; });
-services.AddControllersWithViews();
+services.AddControllers();
 
 var app = builder.Build();
 
-app.UseReact(static config => { });
 app.UseStaticFiles();
 
 app.MapGet("/", static () => "Hello World!");
